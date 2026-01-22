aafffc6c618019bfd94193dfad86d4129312de08
e44 <e446f63@yahoo.com>
2026-01-22 17:42:46 -0600

    New keymap <leader>ts to toggle spellchecker. mason-tool-installer update.

--- init.lua
+++ init.lua
@@ -178,6 +178,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Toggle spell checking
+vim.keymap.set('n', '<leader>ts', '<cmd>lua vim.o.spell = not vim.o.spell<CR>', { desc = '[S]pellchecking' })
+
 --NOTE:
 --[[
 =====================================================================
