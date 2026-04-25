f17aed3f4de99c7d6a3329e4c6b598a6f15fbc80
e44 <e446f63@yahoo.com>
2026-04-25 13:29:09 -0500

    Move lualine and mini.nvim to dedicated modules
    - lualine config now in 'lua/plugins/lualine.lua'
    - mini.nvim config now in 'lua/plugins/mini.lua' (for all mini stuff)
    - Added 'vim.g.active_statusline' variable in 'init.lua' to select
      the active statusline

--- init.lua
+++ init.lua
@@ -98,6 +98,9 @@ vim.g.have_nerd_font = false
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Set statusline. Options: 'lualine', 'mini.statusline'
+vim.g.active_statusline = 'lualine'
+
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
@@ -814,49 +817,6 @@ require('lazy').setup({
     ---@type TodoOptions
     ---@diagnostic disable-next-line: missing-fields
     opts = { signs = false },
-  },
-
-  { -- Collection of various small independent plugins/modules
-    'nvim-mini/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [I]next [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup {
-        -- NOTE: Avoid conflicts with the built-in incremental selection mappings on Neovim>=0.12 (see `:help treesitter-incremental-selection`)
-        mappings = {
-          around_next = 'aa',
-          inside_next = 'ii',
-        },
-        n_lines = 500,
-      }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function() return '%2l:%-2v' end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/nvim-mini/mini.nvim
-    end,
   },
 
   { -- Highlight, edit, and navigate code
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,96 @@
+--[[
+NOTE:
+=====================================================================
+==================== lualine                     ====================
+=====================================================================
+--]]
+
+return {
+
+  --NOTE: lualine
+  {
+    'nvim-lualine/lualine.nvim',
+    enabled = function()
+      --Set this variable in 'init.lua'
+      return vim.g.active_statusline == 'lualine'
+    end,
+    config = function()
+      -- NOTE: Customize inactive statusline and ayu_dark theme
+
+      -- The defaults are at `~/.local/share/nvim/lazy/lualine.nvim/lua/lualine/themes`
+      local custom_ayu_dark = require 'lualine.themes.ayu_dark'
+
+      -- Change inactive window statusline for better separation and darker blue.
+      custom_ayu_dark.inactive.a.bg = '#283d52'
+      custom_ayu_dark.inactive.a.fg = '#36a3d9'
+      custom_ayu_dark.inactive.a.gui = 'bold'
+      custom_ayu_dark.inactive.b.fg = '#36a3d9'
+      custom_ayu_dark.inactive.c.bg = '#283d52'
+      custom_ayu_dark.inactive.c.fg = '#36a3d9'
+
+      -- Change normal mode statusline to blue
+      custom_ayu_dark.normal.c.bg = '#2f93c4'
+      custom_ayu_dark.normal.c.fg = '#14191f'
+      custom_ayu_dark.normal.b.fg = '#36a3d9'
+
+      -- TODO: Someday, update colors of other modes for more consistency. See default theme file above.
+
+      require('lualine').setup {
+
+        -- NOTE: Change this when changing themes!
+        -- Use ayu_dark with above customizations.
+        options = { theme = custom_ayu_dark },
+
+        -- Custom statusline for active sessions.
+        sections = {
+          lualine_a = { 'mode' },
+          -- if no git branch info, insert placeholder text to keep statusline pretty.
+          lualine_b = {
+            {
+              'branch',
+              fmt = function(str)
+                if str == '' or str == nil then
+                  return 'no git repo'
+                end
+                return str
+              end,
+            },
+            'diff',
+            'diagnostics',
+          },
+          lualine_c = { { 'filename', path = 4 } },
+          -- show buffer numbers, not names
+          lualine_x = { { 'buffers', mode = 3 } },
+          lualine_y = { 'fileformat', 'filetype', 'progress' },
+          lualine_z = { 'location' },
+        },
+
+        -- Custom statusline for inactive sessions.
+        -- Same as active, except bg colors.
+        inactive_sections = {
+          lualine_a = { 'mode' },
+          -- if no git branch info, insert placeholder text to keep statusline pretty.
+          lualine_b = {
+            {
+              'branch',
+              fmt = function(str)
+                if str == '' or str == nil then
+                  return 'no git repo'
+                end
+                return str
+              end,
+            },
+            'diff',
+            'diagnostics',
+          },
+          lualine_c = { 'filename' },
+          -- show buffer numbers, not names
+          lualine_x = { { 'buffers', mode = 3 } },
+          lualine_y = { 'fileformat', 'filetype', 'progress' },
+          lualine_z = { 'location' },
+        },
+      }
+    end,
+  },
+
+}
