fe157e5d614971407284c48bf12725533c228da5
e44 <e446f63@yahoo.com>
2026-04-25 00:22:35 -0500

    Comment out Sidekick <TAB> change in blink.cmp
    - Leaving this currently breaks my super-tab completion
    - Also breaks simple <Tab> in Insert mode

--- init.lua
+++ init.lua
@@ -929,19 +929,19 @@ require('lazy').setup(
 
           -- For Sidekick "Integrate <Tab> in insert mode with blink.cmp"
           -- See https://github.com/folke/sidekick.nvim
-          ["<Tab>"] = {
-            "snippet_forward",
-            function() -- sidekick next edit suggestion
-              return require("sidekick").nes_jump_or_apply()
-            end,
-            -- function() -- if you are using Neovim's native inline completions
-            --   return vim.lsp.inline_completion.get()
-            -- end,
-            function() -- if you are using Neovim's native inline completions
-              return vim.lsp.inline_completion.get()
-            end,
-            "fallback",
-          },
+          -- ["<Tab>"] = {
+          --   "snippet_forward",
+          --   function() -- sidekick next edit suggestion
+          --     return require("sidekick").nes_jump_or_apply()
+          --   end,
+          --   -- function() -- if you are using Neovim's native inline completions
+          --   --   return vim.lsp.inline_completion.get()
+          --   -- end,
+          --   function() -- if you are using Neovim's native inline completions
+          --     return vim.lsp.inline_completion.get()
+          --   end,
+          --   "fallback",
+          -- },
 
         },
 
