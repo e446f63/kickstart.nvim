c486bc109f0e4b3a814347fb4f0b20935510a38a
e44 <e446f63@yahoo.com>
2026-04-25 21:29:56 -0500

    Migrate Sidekick plugin without Tab or Blink integration

--- init.lua
+++ init.lua
@@ -285,6 +285,7 @@ require('lazy').setup({
       spec = {
         { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
         { '<leader>t', group = '[T]oggle' },
+        { '<leader>a', group = '[A]gents (Sidekick)' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
         { 'gr', group = 'LSP Actions', mode = { 'n' } },
       },
