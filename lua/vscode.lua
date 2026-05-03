7d9e60d4797eab946b68d62d8e697503c57530c1
e44 <e446f63@yahoo.com>
2026-05-03 16:10:22 -0500

    Add mini.ai, mini.surround, and yank highlights to vscode.lua

--- lua/vscode.lua
+++ lua/vscode.lua
@@ -6,6 +6,21 @@ NOTE:
 --]]
 -- Only load plugins that can work when running inside VS Code
 
-return {
+local mini_path = vim.fn.stdpath 'data' .. '/lazy/mini.nvim'
 
-}
+if vim.uv.fs_stat(mini_path) then
+  vim.opt.rtp:prepend(mini_path)
+
+  require('mini.ai').setup()
+  require('mini.surround').setup()
+end
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank-vscode', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+return {}
